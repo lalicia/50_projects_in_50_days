4dc8bb57f373152bda2c0a7a4e4e23a55b75d903
alicialowe <hohpascal@outlook.com>
2022-12-13 12:59:42 +0000

    added _redirects file. updated favicon and title. tweaker Home wording

--- 0react_50_projects/src/pages/Home.js
+++ 0react_50_projects/src/pages/Home.js
@@ -10,7 +10,7 @@ function Home() {
 
     return (
         <div className="container">
-            <h1>50 Projects in 50 Days</h1>
+            <h1>50 Projects in 50 Days - using React</h1>
             <h2>This app is a translation of Brad Traversy and Florin Pop's 50 HTML, CSS, and JavaScript projects into React versions</h2>
             <h2>Click <a className="home-a" href="https://www.udemy.com/course/50-projects-50-days/" target="_blank" rel="noreferrer">here</a> to visit their course site</h2>
 
