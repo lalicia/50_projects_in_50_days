f67e02e61cc8470669bcad885fdcfb579929d228
alicialowe <hohpascal@outlook.com>
2022-12-14 14:27:39 +0000

    completed FAQ Boxes React translation

--- 0react_50_projects/src/projects/FaqBoxes/Faq.js
+++ 0react_50_projects/src/projects/FaqBoxes/Faq.js
@@ -3,7 +3,7 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
-// import { faComment } from '@fortawesome/pro-regular-svg-icons';
+import { faComment } from '@fortawesome/free-regular-svg-icons';
 
 import "./FaqBoxes.css";
 
@@ -12,6 +12,14 @@ function Faq({title, text, box, setBox}) {
 
     return (
         <div className={box ? "faq-faq faq-active" : "faq-faq"}>
+
+            <FontAwesomeIcon icon={faComment} 
+            className={box ? "faq-comment" : "faq-comment-invisible"}
+            />
+            <FontAwesomeIcon icon={faComment} 
+            className={box ? "faq-comment2" : "faq-comment2-invisible"} 
+            />
+
             <h3 className="faq-faq-title">{title}</h3>
             <p className="faq-faq-text">{text}</p>
             
