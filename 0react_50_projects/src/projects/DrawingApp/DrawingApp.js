e40aaaba18bf4843b1815f7d3dc5cd222c583420
alicialowe <hohpascal@outlook.com>
2022-12-24 13:54:12 +0000

    tweaked css a little for buttons and toolbox border. fixed bug with increase and decrease allowing 0/55

--- 0react_50_projects/src/projects/DrawingApp/DrawingApp.js
+++ 0react_50_projects/src/projects/DrawingApp/DrawingApp.js
@@ -66,13 +66,13 @@ function DrawingApp() {
     function handleClickIncrease() {
         setSize(size + 5)
 
-        if(size > 50) {setSize(50)}
+        if(size >= 50) {setSize(50)}
     }
 
     function handleClickDecrease() {
         setSize(size - 5)
 
-        if(size < 5) {setSize(5)}
+        if(size <= 5) {setSize(5)}
     }
 
     function handleChange(e) {
@@ -113,99 +113,3 @@ function DrawingApp() {
 }
 
 export default DrawingApp;
-
-// const [isPressed, setIsPressed] = useState(false);
-// const [color, setColor] = useState("black");
-// const [x, setX] = useState(undefined);
-// const [y, setY] = useState(undefined)
-
-// const [x2, setX2] = useState(undefined);
-// const [y2, setY2] = useState(undefined);
-
-// const canvasRef = useRef(null);
-// //const ctx = canvasObj.getContext("2d");
-
-// useEffect(() => {
-//     const canvasObj = canvasRef.current;
-//     const ctx = canvasObj.getContext("2d");
-
-//     ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-
-//     drawCircle(ctx, x, y);
-//     drawLine(ctx, x, y, x2, y2);
-// },[drawCircle, drawLine])
-
-
-// function onMouseDown(e) {
-//     setIsPressed(true);
-//     setX(e.offsetX);
-//     setY(e.offsetY);
-// }
-
-// function onMouseUp(e) {
-//     setIsPressed(false);
-//     setX(undefined);
-//     setY(undefined);
-// }
-
-// function onMouseMove(e) {
-//     if(isPressed) {
-//         //taking current positions as move
-//         setX2(e.offsetX);
-//         setY2(e.offsetY);
-
-//         //drawing circle and line at same line to draw with solid lines
-//         drawCircle(x2, y2);
-//         drawLine(x, y, x2, y2);
-
-//         //setting x and y here as move to stop weird line fill effect
-//         setX(x2);
-//         setY(y2);
-//     }
-// }
-
-
-// //these two functions are what the pen draws with simultaneously
-// //MDN Canvas docs on drawing shapes
-// function drawCircle(ctx,x, y) {
-//     ctx.beginPath();
-//     ctx.arc(x, y, size, 0, Math.PI * 2, true);
-//     ctx.fillStyle = color;
-//     ctx.fill();
-// }
-
-// //takes in move from/to, and line from/to
-// function drawLine(ctx, x1, y1, x2, y2) {
-//     ctx.beginPath();
-//     ctx.moveTo(x1, y1);
-//     ctx.lineTo(x2, y2);
-//     ctx.strokeStyle = color;
-//     //this makes line width same as circle radius
-//     ctx.lineWidth = size * 2;
-//     ctx.stroke();
-// }
-
-
-// function handleClickIncrease() {
-//     setSize(size + 5)
-
-//     if(size > 50) {
-//         setSize(50)
-//     }
-// }
-
-// function handleClickDecrease() {
-//     setSize(size - 5)
-
-//     if(size < 5) {
-//         setSize(5)
-//     }
-// }
-
-// function handleChange(e) {
-//     setColor(e.target.value);
-// }
-
-// // function handleClickClear(ctx) {
-// //     ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-// // }
