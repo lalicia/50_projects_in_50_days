6b59fbd083b37c2a4dbe8bd4910c402764acf9d9
alicialowe <hohpascal@outlook.com>
2023-01-04 14:55:41 +0000

    completed React conversion of Live User Filter

--- 0react_50_projects/src/projects/LiveUserFilter/List.js
+++ 0react_50_projects/src/projects/LiveUserFilter/List.js
@@ -2,34 +2,31 @@ import React from "react";
 
 import "./LiveUserFilter.css";
 
-import ListItem from "./List.js";
+import ListItem from "./ListItem.js";
 
-function List({data}) {
-    console.log("this is data List is getting: ",data)
-
+function List({data, searchterm}) {
+    console.log("this is data List is getting: ", data)
 
     return (
-        <ul id="result" className="luf-user-list">
-                    
-            {
-                data.map((user) => {
-                    return <ListItem 
-                key={user.index}
-                firstName={user.name.first} 
-                lastName={user.name.last}
-                locationCity={user.location.city}
-                locationCountry={user.location.country}
-                userImg={user.picture.large}
-                 />
-                } )
-            }
-            
-
-            <li className="luf-loading">
-                <h3>Loading...</h3>
-            </li>
-        </ul>
-
+            data.filter((user) => {
+                return (
+                    user.name.first.toLowerCase().includes(searchterm.toLowerCase())
+                    || user.name.last.toLowerCase().includes(searchterm.toLowerCase())
+                    || user.location.city.toLowerCase().includes(searchterm.toLowerCase())
+                    || user.location.country.toLowerCase().includes(searchterm.toLowerCase())
+                )
+            })
+            .map((user, index) => {
+                //console.log(index)
+                return <ListItem 
+                    key={index}
+                    firstName={user.name.first} 
+                    lastName={user.name.last}
+                    locationCity={user.location.city}
+                    locationCountry={user.location.country}
+                    userImg={user.picture.large}
+                    />
+            })
     )
 }
 
--- 0react_50_projects/src/projects/LiveUserFilter/LiveUserFilter.js
+++ 0react_50_projects/src/projects/LiveUserFilter/LiveUserFilter.js
@@ -5,25 +5,31 @@ import "./LiveUserFilter.css";
 import HomeButton from "../../components/HomeButton.js";
 
 import List from "./List.js";
+//import ListItem from "./List.js";
 
 function LiveUserFilter() {
-    const [userData, setUserData] = useState([]);
+    const [userData, setUserData] = useState(null);
+    const [searchterm, setSearchterm] = useState("")
+
+    const API_URL = "https://randomuser.me/api?results=50";
 
     useEffect(() => {
-        getData();
-    }, [])
+        getData(API_URL);
+        console.log("getData ran")
 
-    async function getData() {
-        const res = await fetch("https://randomuser.me/api?results=50");
-        //destructuring to get data.results
-        const {results} = await res.json();
-        //console.log(data)
-    
-        //Clear result container in UI before do anything
-        setUserData(results)
-    }    
-    
+        async function getData(url) {
+            const res = await fetch(url);
+            
+            const data = await res.json();
+            //console.log(data)
+            const results = data.results;
+        
+            //Clear result container in UI before do anything
+            setUserData(results)
+        }  
+    }, [])
     //console.log(userData)
+
 
     return (
         <div className="luf-big-container">
@@ -32,11 +38,24 @@ function LiveUserFilter() {
                     <h4 className="luf-title">Live User Filter</h4>
                     <small className="luf-subtitle">Search by Name and/or Location</small>
                     
-                    <input type="text" id="filter" placeholder="Search" />
+                    <input type="text" id="filter" placeholder="Search"
+                    onInput={(e) => setSearchterm(e.target.value)}
+                    />
 
                 </header>
 
-                <List data={userData} />
+                <ul id="result" className="luf-user-list">
+    
+                {
+                    userData ? 
+                    <List data={userData} searchterm={searchterm} />
+                    : <li className="luf-loading">
+                        <h3>Loading...</h3>
+                    </li>
+                }
+                    
+                </ul>
+                
 
             </div>
 
@@ -46,3 +65,22 @@ function LiveUserFilter() {
 }
 
 export default LiveUserFilter;
+
+
+
+
+
+//this works of a fashion - badly - will only keep included in userData state so everyone else is deleted, so when you backspace you have no more data coming backfaceVisibility:     function filterData(searchterm) {
+    //     let includes = []
+    //     //console.log(searchterm)
+    //     userData.forEach(user => {
+    //         if(user.name.first.toLowerCase().includes(searchterm.toLowerCase())
+    //         || user.name.last.toLowerCase().includes(searchterm.toLowerCase())
+    //         || user.location.city.toLowerCase().includes(searchterm.toLowerCase())
+    //         || user.location.country.toLowerCase().includes(searchterm.toLowerCase())
+    //         ) {
+    //             includes.push(user)
+    //             setUserData(includes) 
+    //         } 
+    //     })
+    // }
